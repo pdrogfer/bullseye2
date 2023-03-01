f445d2a590ca732f9f9e3b55db6f6e138393de89
Pedro Gonzalez <pdro74@hotmail.com>
2023-03-01 16:16:51 +0100

    Initial Commit

---
+++ Bullseye2/Bullseye2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye2App.swift
+//  Bullseye2
+//
+//  Created by Pedro Gonzalez Ferrandez on 01.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bullseye2
+//
+//  Created by Pedro Gonzalez Ferrandez on 01.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
