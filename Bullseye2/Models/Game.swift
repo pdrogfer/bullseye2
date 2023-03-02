9dab46f6f8840d3845c36901c4544781d5dc8ddf
Pedro Gonzalez <pdro74@hotmail.com>
2023-03-02 14:16:48 +0100

    minor cleanup

--- Bullseye2/Models/Game.swift
+++ Bullseye2/Models/Game.swift
@@ -8,27 +8,14 @@
 import Foundation
 
 struct Game {
-    var target: Int = Int.random(in: 1...100)
-    var score: Int = 0
-    var round: Int = 1
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
     
     func points(sliderValue: Int) -> Int {
-        var difference: Int
-        
-        // Tutorial approach
-//        if sliderValue > target {
-//            difference = sliderValue - target
-//        } else if target > sliderValue {
-//            difference = target - sliderValue
-//        } else {
-//            difference = 0
-//        }
-//
+        var difference = abs(target - sliderValue)
 
-        // simple version
-        difference = abs(target - sliderValue)
-
-        let awardedPoints: Int = 100 - difference
+        let awardedPoints = 100 - difference
         return awardedPoints
     }
 }
